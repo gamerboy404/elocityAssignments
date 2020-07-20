ff2016e22cdfb67a38808d297277af24f8517558
gamerboy404 <37173930+gamerboy404@users.noreply.github.com>
2020-07-19 20:48:40 -0400

    more tests

--- Project/_tests_/LinkedList/linkedList.test.js
+++ Project/_tests_/LinkedList/linkedList.test.js
@@ -5,9 +5,32 @@ describe("a Class that creates a a linked list data type", () => {
     let ll = new LinkedList;
     ll.insertAtHead(100);
     expect((ll.head.data)).toEqual(100);
+   
     ll.insertAtHead(200);
     expect((ll.head.data)).toEqual(200);
-    ll.removeFirstNode();
-    expect((ll.head.data)).toEqual(100);
+    expect((ll.getFirstNode())).toEqual(200);
+
+    
   });
+  it("test 2 insert at Tail", () => {
+    let ll = new LinkedList;
+    ll.insertAtTail(300); 
+    expect((ll.getLastNode())).toEqual(300);
+    
+    ll.insertAtTail(400); 
+    expect((ll.getLastNode())).toEqual(400);
+  
+  });
+  it("test 3 insert at index", () => {
+    let ll = new LinkedList;
+    ll.insertAtIndex(100, 0);
+    ll.insertAtIndex(200, 1);
+    ll.insertAtIndex(300, 2);
+    ll.insertAtIndex(500, 3); 
+    expect((ll.getNodeAtIndex(2))).toEqual(300); 
+    expect((ll.getNodeAtIndex(3))).toEqual(500);
+  
+  });
+
+
 });
